8b2e4800632b0c8256e6342b4f3de7d6ac03ebd6
Eliott-B <eliottb.info@gmail.com>
2024-10-07 16:52:36 +0200

    :bug: | Bloque the admin panel if the user isn't admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -2,8 +2,24 @@ import EnigmaCreation from "../../components/EnigmaCreation/EnigmaCreation";
 import GroupCreation from "../../components/GroupCreation/GroupCreation";
 import SolutionCreation from "../../components/SolutionCreation/SolutionCreation";
 import "./Admin.css";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const Admin = () => {
+    axios.get("/groups/admin", {
+            headers: {
+                "Authorization": "Bearer " + Cookies.get("token")
+            }
+        })
+        .then(response => {
+            if (!response.data.admin) {
+                window.location.href = "/";
+            }
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
+
     return (
         <>
             <h2>Admin page</h2>
